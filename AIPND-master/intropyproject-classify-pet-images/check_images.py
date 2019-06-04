a0dffeb3154dbb14e61057d72eaa003fa33ad2dd
Aegis Liang <algorithm_learning@163.com>
2019-06-04 14:30:59 +0800

    update on 2019-06-04

--- AIPND-master/intropyproject-classify-pet-images/check_images.py
+++ AIPND-master/intropyproject-classify-pet-images/check_images.py
@@ -3,8 +3,8 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Aegis Liang
+# DATE CREATED:  Mar. 30th 2019
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
--- AIPND-master/intropyproject-classify-pet-images/get_pet_labels.py
+++ AIPND-master/intropyproject-classify-pet-images/get_pet_labels.py
@@ -42,4 +42,5 @@ def get_pet_labels(image_dir):
     """
     # Replace None with the results_dic dictionary that you created with this
     # function
+
     return None
